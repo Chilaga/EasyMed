58f7fbcadc55fd641095d89b73cdf27b667dd77c
Leonty <leo.kopytoff@mail.ru>
2019-01-31 20:22:37 +1000

    adding functional of deleting pacients

--- app/src/main/java/com/easymed/leonty/easymed/EditPacient.java
+++ app/src/main/java/com/easymed/leonty/easymed/EditPacient.java
@@ -1,28 +1,36 @@
 package com.easymed.leonty.easymed;
 
 import android.arch.persistence.room.Room;
+import android.content.Intent;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.TextView;
 
 import java.util.List;
 
-public class EditPacient extends AppCompatActivity {
+public class EditPacient extends AppCompatActivity implements DeleteDialogFragment.DeleteDialogListener {
+
+    int id;
+    AppDatabase db;
+    Pacient pacient;
+
+    TextView surname;
+    TextView name;
+    TextView patronymic;
+    TextView dateOfBirth;
+    TextView address;
+    TextView branch;
+    TextView diagnosis;
+    TextView note;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_pacient);
-
-        TextView surname;
-        TextView name;
-        TextView patronymic;
-        TextView dateOfBirth;
-        TextView address;
-        TextView branch;
-        TextView diagnosis;
-        TextView note;
 
         surname = findViewById(R.id.editSurname);
         name = findViewById(R.id.editName);
@@ -33,13 +41,13 @@ public class EditPacient extends AppCompatActivity {
         diagnosis = findViewById(R.id.editDiagnosis);
         note = findViewById(R.id.editNote);
 
-        int id = getIntent().getExtras().getInt("id", 0);
+        id = getIntent().getExtras().getInt("id", 0);
 
-        AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "production")
+        db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "production")
                 .allowMainThreadQueries()
                 .build();
 
-        Pacient pacient = db.pacientDao().loadPacientById(id);
+        pacient = db.pacientDao().loadPacientById(id);
 
         String pSurname = pacient.getSurname();
         String pName = pacient.getName();
@@ -58,9 +66,46 @@ public class EditPacient extends AppCompatActivity {
         branch.setText(pBranch);
         diagnosis.setText(pDiagnosis);
         note.setText(pNote);
+
+        surname.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     public void finishActivity(View view) {
         finish();
+    }
+
+    public void showDeleteDialog(View view) {
+        DialogFragment dialog = new DeleteDialogFragment();
+        dialog.show(getSupportFragmentManager(), "DeleteDialogFragment");
+    }
+
+    // The dialog fragment receives a reference to this Activity through the
+    // Fragment.onAttach() callback, which it uses to call the following methods
+    // defined by the NoticeDialogFragment.NoticeDialogListener interface
+    @Override
+    public void onDialogPositiveClick(DialogFragment dialog) {
+        db.pacientDao().deletePacient(pacient);
+        Intent intent = new Intent(EditPacient.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onDialogNegativeClick(DialogFragment dialog) {
+        // User touched the dialog's negative button
     }
 }
--- app/src/main/java/com/easymed/leonty/easymed/PacientDao.java
+++ app/src/main/java/com/easymed/leonty/easymed/PacientDao.java
@@ -1,8 +1,10 @@
 package com.easymed.leonty.easymed;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
 
 import java.util.List;
 
@@ -13,6 +15,12 @@ public interface PacientDao {
 
     @Query("SELECT * FROM pacient WHERE id = :id LIMIT 1")
     Pacient loadPacientById(int id);
+
+    /*@Update
+    public void updatePacient();*/
+
+    @Delete
+    void deletePacient(Pacient... pacients);
 
     @Insert
     void insertAll(Pacient... pacients);
